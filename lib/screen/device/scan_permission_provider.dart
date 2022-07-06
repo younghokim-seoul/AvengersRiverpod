af5aee87ca7e1168bbf70837d297d98331ec0691
김영호 <ksemfn@gmail.com>
2022-07-06 16:45:41 +0900

    권한 체크 로직 수정..

--- lib/screen/device/scan_permission_provider.dart
+++ lib/screen/device/scan_permission_provider.dart
@@ -1,7 +1,21 @@
+import 'dart:io';
+
+import 'package:device_info_plus/device_info_plus.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:permission_handler/permission_handler.dart';
 
 final locationProvider = FutureProvider<bool>((ref) async {
-  var requestStatus = await Permission.location.request();
-  return requestStatus.isGranted || requestStatus.isLimited;
+  if (Platform.isIOS) return true;
+
+  DeviceInfoPlugin deviceInfo = DeviceInfoPlugin();
+  AndroidDeviceInfo androidInfo = await deviceInfo.androidInfo;
+  if (Platform.isAndroid && androidInfo.version.sdkInt! <= 30) {
+    var requestStatus = await Permission.location.request();
+    return requestStatus.isGranted || requestStatus.isLimited;
+  } else {
+    Map<Permission, PermissionStatus> statuses = await [Permission.bluetoothScan,Permission.bluetoothConnect,Permission.location].request();
+    statuses[Permission.location]?.isGranted;
+    return await Permission.bluetoothScan.isGranted && await Permission.bluetoothConnect.isGranted && await Permission.location.isGranted;
+  }
+
 });
