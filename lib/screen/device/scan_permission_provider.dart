aaad2954c24b92fabaebc21901e4523b6693f72e
김영호 <ksemfn@gmail.com>
2022-07-06 16:46:59 +0900

    테스트 코드 제거

--- lib/screen/device/scan_permission_provider.dart
+++ lib/screen/device/scan_permission_provider.dart
@@ -13,8 +13,7 @@ final locationProvider = FutureProvider<bool>((ref) async {
     var requestStatus = await Permission.location.request();
     return requestStatus.isGranted || requestStatus.isLimited;
   } else {
-    Map<Permission, PermissionStatus> statuses = await [Permission.bluetoothScan,Permission.bluetoothConnect,Permission.location].request();
-    statuses[Permission.location]?.isGranted;
+    await [Permission.bluetoothScan,Permission.bluetoothConnect,Permission.location].request();
     return await Permission.bluetoothScan.isGranted && await Permission.bluetoothConnect.isGranted && await Permission.location.isGranted;
   }
 
